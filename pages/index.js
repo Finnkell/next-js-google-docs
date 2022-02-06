9aaa1e319139fe70d8e1c53908fdf3420114e270
Eric Batista <klose.eric31@gmail.com>
2022-02-06 18:47:52 -0300

    ADD: create GoogleProvider authentication

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,14 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import MyDocuments from '../components/MyDocuments';
 import NewDocument from '../components/NewDocument';
+import Login from '../components/Login';
+import { useSession, getSession } from 'next-auth/client';
 
 export default function Home() {
+  const [session,] = useSession();
+
+  if (!session) return <Login />
+
   return (
     <div>
       <Head>
